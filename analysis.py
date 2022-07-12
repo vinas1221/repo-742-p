06879ff518cfed7f4d2cddf1dedd9fd25b04b632
vinas1221 <benjo_vinas@yahoo.com>
2022-07-12 17:21:15 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -4,3 +4,4 @@ Temporary commit content for 2022-07-03T17:21:15
 Temporary commit content for 2022-07-05T17:21:15
 Temporary commit content for 2022-07-08T17:21:15
 Temporary commit content for 2022-07-11T17:21:15
+Temporary commit content for 2022-07-12T17:21:15
