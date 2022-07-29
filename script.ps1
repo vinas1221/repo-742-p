3cd4760303b3bdde306b4b5c562bed4d2369b624
vinas1221 <benjo_vinas@yahoo.com>
2022-07-29 17:21:15 +0100

    Fixed bug in data processing script

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2022-06-24T17:21:15
 Temporary commit content for 2022-06-27T17:21:15
 Temporary commit content for 2022-07-01T17:21:15
 Temporary commit content for 2022-07-10T17:21:15
+Temporary commit content for 2022-07-29T17:21:15
