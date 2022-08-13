0e0a649cd8d67f1040feeaf8f452cfa3b651a6fe
vinas1221 <benjo_vinas@yahoo.com>
2022-08-13 17:21:15 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -4,3 +4,4 @@ Temporary commit content for 2022-06-27T17:21:15
 Temporary commit content for 2022-07-01T17:21:15
 Temporary commit content for 2022-07-10T17:21:15
 Temporary commit content for 2022-07-29T17:21:15
+Temporary commit content for 2022-08-13T17:21:15
