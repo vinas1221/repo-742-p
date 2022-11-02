8559a19a1cc19c348413eb24f54f7c347ae37880
vinas1221 <benjo_vinas@yahoo.com>
2022-11-02 17:21:15 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -9,3 +9,4 @@ Temporary commit content for 2022-09-13T17:21:15
 Temporary commit content for 2022-09-15T17:21:15
 Temporary commit content for 2022-09-21T17:21:15
 Temporary commit content for 2022-10-28T17:21:15
+Temporary commit content for 2022-11-02T17:21:15
