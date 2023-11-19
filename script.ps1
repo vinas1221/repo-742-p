aa65ae6688e5f21cbb5964baa38deb68534bb214
vinas1221 <benjo_vinas@yahoo.com>
2023-11-19 17:39:03 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -129,3 +129,4 @@ Temporary commit content for 2023-10-26T17:39:03
 Temporary commit content for 2023-11-09T17:39:03
 Temporary commit content for 2023-11-12T17:39:03
 Temporary commit content for 2023-11-17T17:39:03
+Temporary commit content for 2023-11-19T17:39:03
