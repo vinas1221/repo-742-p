d42e97b50b0ed9abd5d25da36f024715d96c6bf1
vinas1221 <benjo_vinas@yahoo.com>
2023-12-03 17:21:15 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -42,3 +42,4 @@ Temporary commit content for 2023-10-14T17:21:15
 Temporary commit content for 2023-10-16T17:21:15
 Temporary commit content for 2023-10-22T17:21:15
 Temporary commit content for 2023-11-14T17:21:15
+Temporary commit content for 2023-12-03T17:21:15
