ffa1a8bb1001df067c7c2292f1bf6a02fc66d0ea
vinas1221 <benjo_vinas@yahoo.com>
2023-12-07 17:39:03 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -134,3 +134,4 @@ Temporary commit content for 2023-11-29T17:39:03
 Temporary commit content for 2023-12-01T17:39:03
 Temporary commit content for 2023-12-03T17:39:03
 Temporary commit content for 2023-12-05T17:39:03
+Temporary commit content for 2023-12-07T17:39:03
