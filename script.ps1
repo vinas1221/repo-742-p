fa279e4e451801cd1338498195a143f9e5c97d70
vinas1221 <benjo_vinas@yahoo.com>
2024-04-28 17:39:03 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -144,3 +144,4 @@ Temporary commit content for 2024-01-30T17:39:03
 Temporary commit content for 2024-02-13T17:39:03
 Temporary commit content for 2024-02-25T17:39:03
 Temporary commit content for 2024-03-07T17:39:03
+Temporary commit content for 2024-04-28T17:39:03
