b2d4c4f98abb2fa0f22d6f9441eda96e019e2fe5
vinas1221 <benjo_vinas@yahoo.com>
2024-09-01 17:39:03 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -178,3 +178,4 @@ Temporary commit content for 2024-07-17T17:39:03
 Temporary commit content for 2024-08-08T17:39:03
 Temporary commit content for 2024-08-22T17:39:03
 Temporary commit content for 2024-08-25T17:39:03
+Temporary commit content for 2024-09-01T17:39:03
