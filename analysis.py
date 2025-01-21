1a4779c3ab7ab82c6914454d0af73d66c8a782d8
vinas1221 <benjo_vinas@yahoo.com>
2025-01-21 17:39:03 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -192,3 +192,4 @@ Temporary commit content for 2024-11-27T17:39:03
 Temporary commit content for 2024-12-19T17:39:03
 Temporary commit content for 2025-01-01T17:39:03
 Temporary commit content for 2025-01-14T17:39:03
+Temporary commit content for 2025-01-21T17:39:03
